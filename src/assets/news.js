77dfb949ccd9256961d3b0967b9076f1423c148d
niikoolaa04 <nikola04jovicic@gmail.com>
2021-07-19 16:43:16 +0200

    News Changes

--- src/assets/news.js
+++ src/assets/news.js
@@ -1,15 +1,11 @@
 let newsArray = [
   {
-    title: "Wooah, something broken? Tell us",
-    text: "Website is currently in Beta Version, if you encounter any issues make sure to report them directly to the Site Developers."    
+    title: "Welcome to our MCLookup | Minecraft Player & Server Informations",
+    text: "Hey! Welcome to our Website, if you find bug make sure to report it to our developers by joining Discord Server (Invite Link in Footer)"    
   },
   {
-    title: "Gotta cool idea? Share it with us",
-    text: "If you get any ideas for new Website Feature contact developers and tell them your idea, if it's good, it may be added to the website."    
-  },
-  {
-    title: "We currently have issues with website",
-    text: "Currently website is encountering issues and our team is looking into problem and it's fix"    
+    title: "You look very smart, gotta idea for a new feature? Let us now.",
+    text: "If you have idea what else we can add to our Website (and is related to 'lookups') contact our team by joining Discord Server (Invite Link in Footer) and then send them message with your idea. Who knows, you may get something?"    
   }
 ];
 

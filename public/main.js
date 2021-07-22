57817670f9d887812c1b1b9f8b9c26559d1b66c5
niikoolaa04 <nikola04jovicic@gmail.com>
2021-07-22 17:49:14 +0200

    User Lookup Responsive done I hope

--- public/main.js
+++ public/main.js
@@ -1,7 +1,9 @@
 console.log('MAIN.JS LOADED SUCCESSFULLY');
 
 function resizeSend() {
-  let doc = document.querySelector('.homeTitle');
+  // let doc = document.querySelector('.homeTitle');
+  // doc.innerHTML = document.documentElement.clientWidth + ' / ' + document.documentElement.clientHeight
+  let doc = document.querySelector('.serverSubtitle');
   doc.innerHTML = document.documentElement.clientWidth + ' / ' + document.documentElement.clientHeight
 }
 

010af34702e3ccbd490d9bc443df572dc1e47bad
niikoolaa04 <nikola04jovicic@gmail.com>
2021-07-25 20:06:09 +0200

    Mongo soon

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,11 +1,20 @@
 const express = require("express");
+require('dotenv')
 const app = express();
 const cookieParser = require("cookie-parser");
 const AuthRouter = require("./auth.js");
+const { Database } = require('quickmongo');
+const config = require("../config.json");
 const path = require("path")
 app.use(cookieParser());
 
 app.use("/auth", AuthRouter);
+
+const db = new Database(config.mongodb);
+
+db.on("ready", () => {
+  console.log("Database connected!");
+});
 
 // app.use(express.static(path.join(__dirname, '../../build')));
 

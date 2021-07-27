5c424c568ec2f34ab8544054426fcac56e2a76a3
niikoolaa04 <nikola04jovicic@gmail.com>
2021-07-27 15:44:51 +0200

    Cards

--- client/src/components/Users/UsersList.js
+++ client/src/components/Users/UsersList.js
@@ -1,32 +1,20 @@
 import {React, useState, useEffect} from 'react'
 import { Link } from 'react-router-dom';
-import axios from 'axios';
 import './listStyle.css'
 
 function UsersList({ users, setUsers }) {
 
-  async function getAllUsers() {
-    await axios.get("http://localhost:3009/database/users", {
-      headers: {
-        "Content-Type": "application/json"
-      }
-    }).then((res) => {
-      setUsers(res.data);
-    });
-  }
-
-  useEffect(() => {
-    getAllUsers()
-  }, [])
-
   return users.map((u, index) => (
+    // column
     <div className="usersCard" key={index}>
-      <img src={u.avatarURL} alt="" className="userlistIcon" />
-      <p className="userlistUsername">{ u.username }#{ u.discriminator }</p>
-      <p className="userlistID">{ u.userID }</p>
-      <Link to={'/profile/' + u.userID} className="linkToUser">
-        <button className="viewUserProfile">View</button>
-      </Link>
+      <div className="card">
+        <img src={u.avatarURL} alt="" className="userlistIcon" />
+        <p className="userlistUsername">{ u.username }#{ u.discriminator }</p>
+        <p className="userlistID">{ u.userID }</p>
+        <Link to={'/profile/' + u.userID} className="linkToUser">
+          <button className="viewUserProfile">View</button>
+        </Link>
+      </div>
     </div>
   ))
 }

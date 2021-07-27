e0e22151f1754f41b004e83c511307cc26d03d9c
niikoolaa04 <nikola04jovicic@gmail.com>
2021-07-27 15:44:51 +0200

    Cards

---
+++ client/src/components/Users/PagionationComponent.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './paginateStyle.css'
+
+function PagionationComponent({ currentPage, nextPage, prevPage }) {
+  return (
+    <div>
+      <div className="pagesContainer">
+        <div className="prevPage" onClick={() => prevPage()}>
+          <i class="fas fa-chevron-left fa-2x"></i>
+        </div>
+        <div className="currentPage">
+          <p className="cPageText">{ currentPage }</p>
+        </div>
+        <div className="nextPage" onClick={() => nextPage()}>
+          <i class="fas fa-chevron-right fa-2x"></i>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PagionationComponent
--- client/src/components/Users/UsersComponent.js
+++ client/src/components/Users/UsersComponent.js
@@ -1,22 +1,67 @@
-import {React, useState} from 'react'
+import {React, useState, useEffect} from 'react'
 import NavbarComponent from '../Navigation/NavbarComponent';
+import FooterComponent from '../Footer/FooterComponent';
+import PagionationComponent from './PagionationComponent';
 import UsersList from './UsersList'
+import axios from 'axios';
 import './style.css'
 
 function UsersComponent() {
 
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [usersPerPage] = useState(20);
+
+  async function getAllUsers() {
+    await axios.get("http://localhost:3009/database/users", {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).then((res) => {
+      setUsers(res.data);
+    });
+  }
+
+  useEffect(() => {
+    getAllUsers()
+  }, [])
+
+  const indexOfLastPost = currentPage * usersPerPage;
+  const indexOfFirstPost = indexOfLastPost - usersPerPage;
+  const currentUsers = users.slice(indexOfFirstPost, indexOfLastPost);
+
+  const nextPage = () => {
+    if(currentPage + 1 > Math.ceil(users.length / usersPerPage)) return;
+    setCurrentPage(currentPage + 1);
+  }
+
+  const prevPage = () => {
+    if(currentPage - 1 == 0) return;
+    setCurrentPage(currentPage - 1);
+  }
 
   return (
     <div>
       <NavbarComponent style={{ zIndex: '5000' }} />
       <div className="userListContainer">
         <div className="userListHero">
+          <h2 className="userListTitle">All Registered Users</h2>
+          <div className="userListSubtitleDiv">
+            <p className="userListSubtitle">List of all Users Registered to our Website.</p>
+          </div>
+          {/* row */}
           <div className="usersListAll">
-            <UsersList users={users} setUsers={setUsers}/>
+            <UsersList users={currentUsers} setUsers={setUsers}/>
+            <PagionationComponent
+              currentPage={currentPage}
+              nextPage={nextPage}
+              prevPage={prevPage}
+            />
           </div>
         </div>
       </div>
+      <FooterComponent />
     </div>
   )
 }

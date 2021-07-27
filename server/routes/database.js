bbc88cd2293688c9c2fb4b95a2096a271d815bc4
niikoolaa04 <nikola04jovicic@gmail.com>
2021-07-27 20:02:00 +0200

    Headers

--- server/routes/database.js
+++ server/routes/database.js
@@ -8,14 +8,14 @@ router.options('*', cors.corsWithOptions, (req, res) => {
 });
 
 router.get('/users', (req, res) => {
-  console.log(req.referer);
+  if(req.headers.request_token != process.env.SERVER_API_KEY) return res.send('No');
   User.find({}).then(function (users) {
     res.json(users);
   });
 });
 
 router.get('/users/:id', (req, res) => {
-  console.log(req.referer);
+  if(req.headers.request_token != process.env.SERVER_API_KEY) return res.send('No');
   User.findOne({userID: req.params.id}, function (err, post) {
     if (err) return console.log(err);
     res.json(post);
@@ -23,13 +23,14 @@ router.get('/users/:id', (req, res) => {
 });
 
 router.put('/users/:id', async function(req, res, next) {
+  if(req.headers.request_token != process.env.SERVER_API_KEY) return res.send('No');
   await User.findOneAndUpdate({ userID: req.params.id }, req.body, function (err, post) {
     res.json(post);
   });
 });
 
 router.post('/newUser', cors.corsWithOptions, function(req, res, next) {
-  console.log(req.referer);
+  if(req.headers.request_token != process.env.SERVER_API_KEY) return res.send('No');
   User.create(req.body)
     .then(data => res.json(data))
     .catch(next)

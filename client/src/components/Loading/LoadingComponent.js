33a9cb66d221c4c722d10178e7d8303c1dd24e8d
niikoolaa04 <nikola04jovicic@gmail.com>
2021-08-02 11:33:04 +0200

    Profile Responsive, Snackbar

--- client/src/components/Loading/LoadingComponent.js
+++ client/src/components/Loading/LoadingComponent.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './style.css'
 
-function LoadingComponent() {
+function LoadingComponent(props) {
   return (
     <div>
-      <div className="loadingContainer">
+      <div className="loadingContainer" style={{ marginTop: props.marginFix }}>
         <p className="loadingText">Content is loading, please wait..</p>
       </div>
     </div>

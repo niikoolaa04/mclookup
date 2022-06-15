26ec98e462df68a187e77e7516067e3808e4a9be
niikoolaa04 <nikola04jovicic@gmail.com>
2022-06-15 13:01:08 +0200

    Test last

--- server/server.js
+++ server/server.js
@@ -11,14 +11,22 @@ app.set("trust proxy", 1)
 app.use(cookieParser());
 app.use(cors({
   credentials: true,
-  origin: true,
+  origin: [
+    "https://mclookup-niikoolaa04.vercel.app",
+    "https://discordapp.com",
+    "https://discord.com",
+    "https://discordapp.com/",
+    "https://discord.com/",
+  ],
 }));
 app.use(express.json())
 
-app.use(function(req, res, next) {  
-  res.header('Access-Control-Allow-Origin', req.headers.origin);
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+app.use(function(req, res, next) {     
+  res.header('Access-Control-Allow-Credentials', true);     
+  res.header('Access-Control-Allow-Origin', req.headers.origin);     
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');     
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  next(); 
 });  
 
 // app.use((req, res, next) => {
